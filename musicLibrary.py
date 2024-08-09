881200a5b818a0ba1e945dfbe4d2026687653824
Sibtain Raza <87526801+Sibtain24@users.noreply.github.com>
2024-08-09 22:13:14 +0530

    Update musicLibrary.py

--- musicLibrary.py
+++ musicLibrary.py
@@ -1,5 +1,5 @@
 # Creating a music library to store the links of favorite songs and playlists
 # Replace song_name with the name of the song
-# Replace paste link with the link of your song or playlist
+# Replace paste_link with the link of your song or playlist
 
-music = {"wanted" : "https://youtu.be/5n_Z9rGPs6s?si=VhikrotAl2wagG7-", "song_name" : "paste link"}
+music = {"wanted" : "https://youtu.be/5n_Z9rGPs6s?si=VhikrotAl2wagG7-", "song_name" : "paste_link"}
